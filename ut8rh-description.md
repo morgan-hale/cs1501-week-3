b10dd1077fa492a9cb36c47625e90cf60a0b8b95
Umika <ut8rh@virginia.edu>
2021-09-30 00:00:37 -0400

    new py and md changes

---
+++ ut8rh-description.md
@@ -0,0 +1,7 @@
+Umika Tunuguntla ut8rh
+
+https://leetcode.com/problems/length-of-last-word/
+
+Given a string s consisting of some words separated by some number of spaces, return the length of the last word in the string.
+
+A word is a maximal substring consisting of non-space characters only.
---
+++ ut8rh-leetcode.py
@@ -0,0 +1,15 @@
+# Umika Tunuguntla ut8rh
+
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        
+        arr= []
+        
+        arr= (s.strip()).split(" ")
+        index = len(arr) - 1
+        return len(arr[index])
+        
